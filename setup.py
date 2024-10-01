283db669d8097d9e6968c65e92be587f35b2de7e
Sumityg <sumit.yg@gmail.com>
2024-10-01 18:48:58 +0530

    Cookiecutter Initial Commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Top 3 recommendation for food items',
+    author='Sumit',
+    license='',
+)
